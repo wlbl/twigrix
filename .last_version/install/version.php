3525529353f3786c649ecc5cc3ddebe2f6437c08
dmnbars <borisov.a@wlbl.ru>
2016-07-11 12:22:15 +0300

    Поддержка composer

--- .last_version/install/version.php
+++
@@ -1,6 +0,0 @@
-<?php
-$arModuleVersion = array(
-    "VERSION" => "0.9.3",
-    "VERSION_DATE" => "2016-07-10 23:55:00",
-);
-?>
---
+++ install/version.php
@@ -0,0 +1,6 @@
+<?php
+$arModuleVersion = array(
+	"VERSION" => "0.9.5",
+	"VERSION_DATE" => "2016-07-11 12:28:00",
+);
+?>
